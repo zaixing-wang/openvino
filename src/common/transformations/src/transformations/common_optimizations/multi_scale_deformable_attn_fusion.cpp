741e79fc49c5817707258ff56f9355ae1351ec29
ceciliapeng2011 <cecilia.peng@intel.com>
2025-07-04 15:38:52 +0800

    fix

--- src/common/transformations/src/transformations/common_optimizations/multi_scale_deformable_attn_fusion.cpp
+++ src/common/transformations/src/transformations/common_optimizations/multi_scale_deformable_attn_fusion.cpp
@@ -62,8 +62,8 @@ std::shared_ptr<ov::Node> grid_sample_block(const std::shared_ptr<ov::Node>& inp
     auto attn_Reshape_5 = wrap_type<Reshape>({attn_Transpose, any_input()});
     
     auto attn_Gather_9 = wrap_type<Gather>({input_attn_offsets, any_input(), any_input()});
-    auto attn_squeeze = std::make_shared<Squeeze>(attn_Gather_9, any_input());  // FIXME???
-    auto attn_Transpose_1 = wrap_type<Transpose>({attn_squeeze, any_input()});
+    auto attn_squeeze_optional = optional<Squeeze>({attn_Gather_9, any_input()});
+    auto attn_Transpose_1 = wrap_type<Transpose>({attn_squeeze_optional, any_input()});
     auto attn_Reshape_6 = wrap_type<Reshape>({attn_Transpose_1, any_input()});
 
     auto attn_GridSample = wrap_type<GridSample>({attn_Reshape_5, attn_Reshape_6});
